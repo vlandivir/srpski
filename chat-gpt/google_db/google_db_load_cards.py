2b2879bfe8ada0f600fc0e1cab216fca47e93fa0
Vladimir Rybakov <vladimir.rybakov@gmail.com>
2024-03-05 15:35:25 +0100

    small refactoring

--- chat-gpt/google_db/google_db_load_cards.py
+++ chat-gpt/google_db/google_db_load_cards.py
@@ -1,18 +1,14 @@
-import os
 import json
 import time
-import gspread
 
 from sys import getsizeof
 from pympler import asizeof
 
 from oauth2client.service_account import ServiceAccountCredentials
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-service_account_file = os.path.join(current_dir, '..', '..', 'keys', 'srpski-data-e364d16a7d45.json')
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(service_account_file, scope)
-client = gspread.authorize(creds)
+from google_db_api import get_gspread_client
+
+client = get_gspread_client()
 
 sheet_id = '1rnD7wWNl8NqyKL3amafrArzHmapA1QrrsB7PAAWR-7w'
 sheet_name = 'Cards'
